193744d70852ca974cd10190f83d0c8e8a9060e1
cheenuh <123313420+cheenuh@users.noreply.github.com>
2023-02-13 12:52:03 +0800

    Initial commit

---
+++ Midterms/firstapp.js
@@ -0,0 +1,10 @@
+const { response } = require('express');
+var express = require('express');
+var app = express();
+app.get('/', function (request, respone) {
+  response.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Server running at http://localhost:3000');
+});
